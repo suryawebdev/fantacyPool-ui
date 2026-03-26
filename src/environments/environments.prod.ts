5fb8858e8f9dc877376b7ae6d9d9f8f05d6d95dc
Suryapweb <suryapweb.com>
2026-03-25 23:44:13 -0500

    updated api urls

--- src/environments/environments.prod.ts
+++ src/environments/environments.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:8080',
-  // apiUrl: 'https://fp-api-production-36dd.up.railway.app',
+  // apiUrl: 'http://localhost:8080',
+  apiUrl: 'https://fp-api-production-36dd.up.railway.app',
   // apiUrl: 'http://fantacypool-api-env.eba-c8dpmph3.us-east-2.elasticbeanstalk.com',
   // WebSockets disabled — set true and uncomment websocket code to re-enable
   enableWebSockets: false,
